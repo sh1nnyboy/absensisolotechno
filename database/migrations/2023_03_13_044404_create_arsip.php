a417283f6e16fce739d15da1d3e5fc97ca1a1501
unknown <hasan.2001@students.amikom.ac.id>
2023-03-13 11:47:26 +0700

    create arsip

---
+++ database/migrations/2023_03_13_044404_create_arsip.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArsip extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('arsip', function (Blueprint $table) {
+            $table->id();
+            $table->string('kk');
+            $table->string('ktp');
+            $table->string('ijazah');
+            $table->string('sertifikat');
+            $table->string('suratrekomendasi');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('arsip');
+    }
+}
